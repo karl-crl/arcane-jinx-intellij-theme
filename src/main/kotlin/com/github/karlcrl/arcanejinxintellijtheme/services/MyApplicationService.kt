1e6e83aee6859c6cb21c576b501168c69f528638
GitHub Action <action@github.com>
2022-06-29 15:12:20 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/karlcrl/arcanejinxintellijtheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.karlcrl.arcanejinxintellijtheme.services
+
+import com.github.karlcrl.arcanejinxintellijtheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
